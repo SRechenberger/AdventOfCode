a2b465e6d446f374dc32c4394126bd97122cdeb9
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2022-12-12 09:14:39 +0100

    +10

---
+++ 2022/10/part1.hs
@@ -0,0 +1,34 @@
+import Control.Arrow
+
+data Command = NoOp | Addx Int | AddWait
+  deriving (Show, Eq)
+
+readCommand :: String -> Command
+readCommand "noop" = NoOp
+readCommand addx   = Addx . read . tail . dropWhile (/=' ') $ addx
+
+cycles :: Command -> Int
+cycles NoOp     = 1
+cycles (Addx _) = 2
+
+expand :: [Command] -> [Command]
+expand = map e >>> concat
+  where
+    e NoOp     = [NoOp]
+    e (Addx i) = [AddWait, Addx i]
+
+eval :: (Int, Int) -> Command -> (Int, Int)
+eval (i, c) (Addx d) = (i + d, c+1)
+eval (i, c) _        = (i,     c+1)
+
+compute :: String -> Int
+compute = lines
+  >>> map readCommand
+  >>> expand
+  >>> scanl eval (1,1)
+  >>> filter (snd >>> (`elem` [20,60..220]))
+  >>> map (uncurry (*))
+  >>> sum
+
+main :: IO ()
+main = compute <$> getContents >>= print
